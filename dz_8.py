42252c414b9f6b25221e24aa9714552f91fdb84c
Andrij-Latkivskyj <latkivskijandrij@gmail.com>
2023-04-25 21:17:47 +0300

    Update dz_8.py

--- dz_8.py
+++ dz_8.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 
 
 users = [
@@ -8,8 +8,8 @@ users = [
     { 'name' : 'Petro', 'birthday' : datetime(year = 1990, month = 4, day = 30 )},
     { 'name' : 'Volodya', 'birthday' : datetime(year = 1989, month = 1, day = 22 )},
     { 'name' : 'Mykola', 'birthday' : datetime(year = 1991, month = 4, day = 26 )},
-    { 'name' : 'Myhaylo', 'birthday' : datetime(year = 1995, month = 2, day = 26 )},
-    { 'name' : 'Georgij', 'birthday' : datetime(year = 1995, month = 4, day = 24 )}
+    { 'name' : 'Myhaylo', 'birthday' : datetime(year = 1995, month = 4, day = 26 )},
+    { 'name' : 'Georgij', 'birthday' : datetime(year = 1995, month = 4, day = 27 )}
 ]
 
 
@@ -32,45 +32,60 @@ def week():
 
 #------------------------------------------------------------------------------------------
 
-days_name = {
-    0 : 'Понеділок',
-    1 : 'Вівторок',
-    2 : 'Середа',
-    3 : 'Четвер',
-    4 : "П'ятниця",
-    5 : 'Субота',
-    6 : 'Неділя'
-}
+def print_name_bd(sp):
+    if not sp:
+        return 'no birthdays'
+    else:
+        name = ' '
+        for i in sp:
+            name  += i + ' '
+        return name
 
 #------------------------------------------------------------------------------------------
 
 def get_birthday_per_week(data):
-    
+    tuesday = []
+    wednesday = []
+    thursday = []
+    friday = []
+    monday = []
+    today = datetime.now()
+
     for sl in data:
         
         name, bd = sl.values()
-            
-        for d in week():
-
-            if d.strftime('%d/%m') == bd.strftime('%d/%m'):
+              
+        bd = bd.replace(year = today.year)
                 
-                if days_name.get(d.weekday()) == days_name[1]:
-                    print(f'{days_name.get(d.weekday())} - {name}')
+        if bd.date() in week():
+            
+            if bd.weekday() == 1:
+                tuesday.append(name)
+                
+            elif bd.weekday() == 2:
+                wednesday.append(name)
+                
+            elif bd.weekday() == 3:
+                thursday.append(name)
+                
+            elif bd.weekday() == 4:
+                friday.append(name)
+                
+            else:
+                monday.append(name)
 
-                elif days_name.get(d.weekday()) == days_name[2]:
-                    print(f'{days_name.get(d.weekday())} - {name}')
-                    
-                elif days_name.get(d.weekday()) == days_name[3]:
-                    print(f'{days_name.get(d.weekday())} - {name}')
+    
 
-                elif days_name.get(d.weekday()) == days_name[4]:
-                    print(f'{days_name.get(d.weekday())} - {name}')
+    print(f'Monday: {print_name_bd(monday)}\nTuesday: {print_name_bd(tuesday)}\nWednsday: {print_name_bd(wednesday)}\nThursday: {print_name_bd(thursday)}\nFriday: {print_name_bd(friday)}')
 
-                else:
-                    print(f'Понеділок - {name}')
+
+
+        
+            
     
     
 if __name__ == '__main__':
 
     get_birthday_per_week(users)
+
 
